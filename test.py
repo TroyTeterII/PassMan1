b962c1ab9c6676e8c4dbb3dd96811dc7e79b5678
Troy Teter <troyteter00@gmail.com>
2023-05-16 01:05:23 -0400

    initial commit

---
+++ test.py
@@ -0,0 +1,104 @@
+import sqlite3
+import hashlib
+import tkinter as tk
+from tkinter import messagebox
+
+
+print('launching passman')
+
+#database and table
+dbName='passwords.db'
+tableName='users'
+
+#db connection
+conn=sqlite3.connect(dbName)
+c=conn.cursor()
+
+#table check
+c.execute('''CREATE TABLE IF NOT EXISTS {} (username text, password text)'''.format(tableName))
+conn.commit()
+
+#add new user
+def addUser(user, password):
+    hashPass=hashlib.sha256(password.encode()).hexdigest()
+    c.execute('INSERT INTO {} VALUES (?,?)'.format(tableName), (user, hashPass))
+    conn.commit()
+
+#simple user authentication
+def authUser(user, password):
+    hashPass=hashlib.sha256(password.encode()).hexdigest()
+    c.execute('SELECT * FROM {} WHERE username=? AND password =?'.format(tableName), (user, hashPass))
+    result=c.fetchone()
+    if result is not None:
+        return True
+    else:
+        return False
+#create gui
+class Application(tk.Frame):
+    def __init__(self,master=None):
+        super().__init__(master)
+        self.master=master
+        self.master.title('Pass Man 0.1')
+        self.master.geometry('400x200')
+        self.master.resizable(False,False)
+        self.configure(bg='#f5f5f5')
+        self.create_widgets()
+        
+        #buttons
+    def create_widgets(self):
+        # username
+        self.username_label = tk.Label(self, text='Username:', font=('Helvetica', 12), bg='#f5f5f5')
+        self.username_label.pack(pady=(20, 0))
+
+        self.username_entry = tk.Entry(self, font=('Helvetica', 12), bg='#ffffff')
+        self.username_entry.pack()
+
+        # password
+        self.password_label = tk.Label(self, text='Password:', font=('Helvetica', 12), bg='#f5f5f5')
+        self.password_label.pack(pady=(10, 0))
+
+        self.password_entry = tk.Entry(self, font=('Helvetica', 12), show='*', bg='#ffffff')
+        self.password_entry.pack()
+
+        # register button
+        self.button_frame=tk.Frame(self, bg='#f5f5f5')
+        self.button_frame.pack(pady=(20,0))
+        
+        self.register_button = tk.Button(self, text='Register', font=('Helvetica', 12), bg='#007bff', fg='#ffffff', command=lambda: self.register_user())
+        self.register_button.pack(side=tk.LEFT)
+
+        #login button
+        self.login_button=tk.Button(self.button_frame,text='Login', font=('Helvetica',12), bg='#28a745', fg='#ffffff', command=lambda: self.login_user())
+        self.login_button.pack(side=tk.LEFT,padx=(0,10))
+
+    #register user
+    def register_user(self):
+        user=self.username_entry.get()
+        passw=self.password_entry.get()
+        if user and passw:
+            addUser(user, passw)
+            self.username_entry.delete(0,tk.END)
+            self.password_entry.delete(0,tk.END)
+            tk.messagebox.showinfo('success', 'registration successful')
+        else:
+            tk.messagebox.showerror('error', 'please enter username and password.')
+
+    #login existing user
+    def login_user(self):
+        user=self.username_entry.get()
+        passw=self.password_entry.get()
+        if user and passw:
+            if authUser(user,passw):
+                tk.messagebox.showinfo('Success', 'Login Successful')
+            else:
+                tk.messagebox.showerror('error', 'incorrect combo')
+        else:
+            tk.messagebox.showerror('error', 'please enter user and passw')
+        self.username_entity.delete(0,tk.END)
+        self.password_entity.delete(0,tk.END)
+
+#main
+root=tk.Tk()
+app=Application(master=root)
+app.pack(fill='both',expand=True)
+app.mainloop()

b7e56673f7f2b6a6be8ed0f662ecea9b412faf87
Troy Teter <troyteter00@gmail.com>
2023-05-17 15:20:10 -0400

    added a new button that generates random secure passwords

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import sqlite3
 import hashlib
 import tkinter as tk
 import re
+import random
+import string
 from tkinter import messagebox
 from tkinter import simpledialog
 from urllib.parse import urlparse
@@ -92,7 +94,6 @@ def valid_url(url):
 class Login(tk.Frame):
     def __init__(self,master=None):
         super().__init__(master)
-
         #some visual elements to the GUI
         self.master=master
         self.master.title('Pass Man 0.1')
@@ -100,30 +101,23 @@ class Login(tk.Frame):
         self.master.resizable(False,False)
         self.configure(bg='#f5f5f5')
         self.create_widgets()
-        
         #buttons
     def create_widgets(self):
         # username display
         self.username_label = tk.Label(self, text='Email:', font=('Helvetica', 12), bg='#f5f5f5')
         self.username_label.pack(pady=(20, 0))
-
         self.username_entry = tk.Entry(self, font=('Helvetica', 12), bg='#ffffff')
         self.username_entry.pack()
-
         # password display
         self.password_label = tk.Label(self, text='Password:', font=('Helvetica', 12), bg='#f5f5f5')
         self.password_label.pack(pady=(10, 0))
-
         self.password_entry = tk.Entry(self, font=('Helvetica', 12), show='*', bg='#ffffff')
         self.password_entry.pack()
-
         # register button display
         self.button_frame=tk.Frame(self, bg='#f5f5f5')
         self.button_frame.pack(pady=(20,0))
-        
         self.register_button = tk.Button(self, text='Register', font=('Helvetica', 12), bg='#007bff', fg='#ffffff', command=lambda: self.register_user())
         self.register_button.pack(side=tk.LEFT, anchor=tk.SW)
-
         #login button display
         self.login_button=tk.Button(self.button_frame,text='Login', font=('Helvetica',12), bg='#28a745', fg='#ffffff', command=lambda: self.login_user())
         self.login_button.pack(side=tk.LEFT,padx=(0,10))
@@ -150,11 +144,14 @@ class Login(tk.Frame):
         user=self.username_entry.get()
         passw=self.password_entry.get()
 
+        #check for both fields to have data
         if user and passw:
+            #call authUser to validate login info in passwords.db
             if authUser(user,passw):
                 tk.messagebox.showinfo('Success', 'Login Successful')
                 create_database(user)
                 self.destroy()
+                #load home page
                 home= HomePage(self.master)
                 home.pack(fill='both',expand=True)
                 return
@@ -180,49 +177,67 @@ class HomePage(tk.Frame):
 
     def create_widgets(self):
         #welcome label
-        self.welcome=tk.Label(self, text='Welcome')
+        self.welcome=tk.Label(self, text='Home Page')
         self.welcome.pack()
-
         button_frame=tk.Frame(self)
         button_frame.pack(side=tk.LEFT)
         
         #new entry button
-        self.entry=tk.Button(self,text='New Entry',command=self.entry_page)
+        self.entry=tk.Button(self,text='Add a New Password',command=self.entry_page)
         self.entry.pack(side=tk.TOP,anchor=tk.NW)
+
+        #generate password button
+        self.entry=tk.Button(self,text='Generate A New Password',command=self.generate_page)
+        self.entry.pack(side=tk.TOP,anchor=tk.W)
 
         #logout button
         self.logout=tk.Button(self,text='Logout', command=self.logout_page)
         self.logout.pack(side=tk.BOTTOM,anchor=tk.SW)
 
-    #logout fuctnion
+    #logout function
     def logout_page(self):
         self.destroy()
         login_page=Login(self.master)
         login_page.pack(fill='both', expand=True)
 
+    #generate password popup
+    def generate_page(self):
+        uppers=string.ascii_uppercase
+        lowers=string.ascii_lowercase
+        digits=string.digits
+        spchars=string.punctuation
+        totalchar=uppers+lowers+digits+spchars
+        length=12
+        secpass=''.join(random.choices(totalchar,k=length))
+
+        pop_win=tk.Tk()
+        pop_entry=tk.Entry(pop_win,readonlybackground='white')
+        pop_entry.insert(0,secpass)
+        pop_entry.pack(fill='both',expand=True)
+        pop_entry.configure(state='readonly')
+
+
+
+
     #new entry popup 
     def entry_page(self):
         entry_input=tk.Toplevel(self)
         entry_input.title('New Entry')
-
          # Website
         website_label = tk.Label(entry_input, text='Website:')
         website_label.grid(row=0, column=0, sticky=tk.E)
         website_entry = tk.Entry(entry_input)
         website_entry.grid(row=0, column=1, padx=5,pady=5)
-
         # Username
         user_label = tk.Label(entry_input, text='Username:')
         user_label.grid(row=1, column=0, sticky=tk.E)
         user_entry = tk.Entry(entry_input)
         user_entry.grid(row=1, column=1, padx=5, pady=5)
-
         # Password
         pass_label = tk.Label(entry_input, text='Password:')
         pass_label.grid(row=2, column=0, sticky=tk.E)
         pass_entry = tk.Entry(entry_input, show='*')
         pass_entry.grid(row=2, column=1, padx=5,pady=5)
-
         # Submit button
         submit_button = tk.Button(entry_input, text='Submit New Entry', command=lambda: self.submit_page(website_entry.get(), user_entry.get(), pass_entry.get()))
         submit_button.grid(row=3, column=0, columnspan=2,padx=5,pady=10)
@@ -239,6 +254,8 @@ class HomePage(tk.Frame):
             create_table(website,user,password)
             tk.messagebox.showinfo('Success', 'Entry added to db')
             self.destroy()
+            home=HomePage(self.master)
+            home.pack(fill='both',expand=True)
         else:
             tk.messagebox.showerror('error','please fill in all fields')
 
